8f4524b277e6c63276aa956f911202492a7bffca
Antoine-Beaudry0 <antoine.beaudry.01@edu.cegeptr.qc.ca>
2023-03-24 12:59:45 -0400

    save de finPartie et gestion Jeu (j'ai des problèmes de comptage et de temps)

--- Assets/_MainProject/_Scripts/Gestion/GestionJeu.cs
+++ Assets/_MainProject/_Scripts/Gestion/GestionJeu.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GestionJeu : MonoBehaviour
@@ -8,7 +6,9 @@ public class GestionJeu : MonoBehaviour
 
     private int _pointage = 0;  // Attribut qui conserve le nombre d'accrochages
     private int _accrochageNiveau1 = 0;  // Atribut qui conserve le nombre d'accrochage pour le niveau 1
+    private int _accrochageNiveau2 = 0;
     private float _tempsNiveau1 = 0.0f;  // Attribut qui conserve le temps pour le niveau 1
+    private float _tempsNiveau2 = 0.0f;
 
     // ***** Méthodes privées *****
     private void Awake()
@@ -40,7 +40,6 @@ public class GestionJeu : MonoBehaviour
         Debug.Log("*** Course à obstacles");
         Debug.Log("Le but du jeu est d'atteindre la zone d'arrivée le plus rapidement possible");
         Debug.Log("Chaque contact avec un obstable entraînera une pénalité");
-        Debug.Log("");
     }
 
     // ***** Méthodes publiques ******
@@ -65,10 +64,19 @@ public class GestionJeu : MonoBehaviour
         return _tempsNiveau1;
     }
 
+    public float GetTempsNiv2()
+    {
+        return _tempsNiveau2;
+    }
     // Accesseur qui retourne le nombre d'accrochages pour le niveau 1
     public int GetAccrochagesNiv1()
     {
         return _accrochageNiveau1;
+    }
+
+    public int GetAccrochagesNiv2()
+    {
+        return _accrochageNiveau2;
     }
 
     // Méthode qui reçoit les valeurs pour le niveau 1 et qui modifie les attributs respectifs
@@ -76,5 +84,10 @@ public class GestionJeu : MonoBehaviour
     {
         _accrochageNiveau1 = accrochages;
         _tempsNiveau1 = tempsNiv1;
+    }
+    public void SetNiveau2(int accrochages, float tempsNiv2)
+    {
+        _accrochageNiveau2 = accrochages - _accrochageNiveau1;
+        _tempsNiveau2 = tempsNiv2 - _tempsNiveau1;
     }
 }
